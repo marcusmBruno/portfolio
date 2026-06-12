3b2598d59a6860ffa0e881d35fedc7b30d844e71
Marcus Bruno <139879334+marcusmBruno@users.noreply.github.com>
2026-06-12 14:40:59 +0800

    remove unused imports, improve conditional rendering logic and add missing font type declarations

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { motion, AnimatePresence, delay } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 type ImageSliderProps = {
     images: string[]
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,17 +4,17 @@ export function NavBar({ pathName }: { pathName: string }) {
     return (
         <header className="my-5">
             <ul className="flex gap-6 list-none text-zinc-400 font-medium">
-                <li className={pathName === '/' && "text-zinc-100"}>
+                <li className={pathName === '/' ? "text-zinc-100" : undefined}>
                     <Link to="/">
                         home
                     </Link>
                 </li>
-                <li className={pathName === '/projects' && "text-zinc-100"}>
+                <li className={pathName === '/projects' ? "text-zinc-100" : undefined}>
                     <Link to="projects">
                         projects
                     </Link>
                 </li>
-                <li className={pathName === '/about-me' && "text-zinc-100"}>
+                <li className={pathName === '/about-me' ? "text-zinc-100" : undefined}>
                     <Link to="about-me">
                         about me
                     </Link>
